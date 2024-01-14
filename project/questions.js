b7bc84fcd73b0d963ce733948b2a3a805fcba94a
somya2213148pal <somya2213148@akgec.ac.in>
2024-01-14 17:06:14 +0530

    next commit

---
+++ project/questions.js
@@ -0,0 +1,9 @@
+const questions = [
+    {
+        question: "What is the capital of France?",
+        options: ["London", "Berlin", "Paris"],
+        answer: "Paris",
+        questions: "how to comment a html tag?",
+        options: ["/*--*/", "//", "!--"],
+    }
+];
